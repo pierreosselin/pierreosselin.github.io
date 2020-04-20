5a32722e90c25c885410f5ec2a68b2463ea32864
pierreosselin <pierre.osselin@gmail.com>
2020-04-20 11:46:57 +0200

    Add linesearch and documentation

--- _site/assets/js/documentationDisplay.js
+++ _site/assets/js/documentationDisplay.js
@@ -1,10 +1,38 @@
 
 // Display the documentation
-function documentationFactory(paramName, doc, containerId) {
+function documentationFactoryFunction(paramName, doc, containerId) {
   let div = d3.select(containerId).html("");
   div.append("p").style("color", "black").style("font-size", "50px").text(doc[paramName].name);
   let formulaP = div.append("p");
   let text = formulaP.append("foreignObject").attr("width",50).attr("height",50)
   text.text(doc[paramName].value);
   MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+
+}
+
+// Display the documentation
+function documentationFactoryAlgo(paramName, doc, containerId) {
+  let div = d3.select(containerId).html("");
+  div.append("p").style("color", "black").style("font-size", "50px").text(doc[paramName].name);
+  let formulaP = div.append("p");
+  let text = formulaP.append("foreignObject").attr("width",50).attr("height",50)
+  text.text(doc[paramName].value);
+  d3.select(containerId).append('img').attr("src", doc[paramName].link).attr("class", "svg-content-responsive-algo")
+  let formulaExplaination = div.append("p");
+  let textExplaination = formulaExplaination.append("foreignObject").attr("width",50).attr("height",50)
+  textExplaination.text(doc[paramName].explain);
+  MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+}
+
+// Display the documentation
+function documentationFactoryBarmijo(flag, doc, containerId) {
+  let div = d3.select(containerId).html("");
+  if (flag) {
+    div.append("p").style("color", "black").style("font-size", "50px").text(doc.name);
+    let formulaP = div.append("p");
+    let text = formulaP.append("foreignObject").attr("width",50).attr("height",50)
+    text.text(doc.explain);
+    d3.select(containerId).append('img').attr("src", doc.link).attr("class", "svg-content-responsive-algo")
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+  }
 }
